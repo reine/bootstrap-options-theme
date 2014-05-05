03290fced5a24cdef16ea4f34b22f9a7f1b60fb0
Reine Sison <reawakened.phoenix@gmail.com>
2014-05-05 11:33:45 +0800

    Updated header & footer files with modified subpackage names.

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The Header for our theme.
+ * The template for displaying the header.
  *
  * @package WordPress
  * @subpackage Bootstrap Options Theme
@@ -9,11 +9,12 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
-	<div id="page">
+	<div id="page" class="container-fluid">
