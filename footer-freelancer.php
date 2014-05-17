850b7abf92536cee79cf4301711f1b050689fb43
Reine Sison <reawakened.phoenix@gmail.com>
2014-05-17 16:11:10 +0800

    Implemented SNS icons & links inside the Freelancer footer.

--- footer-freelancer.php
+++ footer-freelancer.php
@@ -13,22 +13,44 @@
                 <div class="row">
                     <div class="footer-col col-md-4">
                         <h3>Location</h3>
-                        <p>3481 Melrose Place
-                            <br>Beverly Hills, CA 90210</p>
+                        <p>3481 Melrose Place<br>Beverly Hills, CA 90210</p>
                     </div>
                     <div class="footer-col col-md-4">
                         <h3>Around the Web</h3>
                         <ul class="list-inline">
-                            <li><a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+
+                            <?php if ( of_get_option('facebook_url_text') != '' ): ?>
+                            <li>
+                                <a href="<?php echo of_get_option('facebook_url_text'); ?>" class="btn-social btn-outline" target="_blank">
+                                    <i class="fa fa-fw fa-facebook"></i>
+                                </a>
                             </li>
-                            <li><a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+                            <?php endif; ?>
+
+                            <?php if ( of_get_option('google_plus_url_text') != '' ): ?>
+                            <li>
+                                <a href="<?php echo of_get_option('google_plus_url_text'); ?>" class="btn-social btn-outline" target="_blank">
+                                    <i class="fa fa-fw fa-google-plus"></i>
+                                </a>
                             </li>
-                            <li><a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+                            <?php endif; ?>
+
+                            <?php if ( of_get_option('twitter_url_text') != '' ): ?>
+                            <li>
+                                <a href="<?php echo of_get_option('twitter_url_text'); ?>" class="btn-social btn-outline" target="_blank">
+                                    <i class="fa fa-fw fa-twitter"></i>
+                                </a>
                             </li>
-                            <li><a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+                            <?php endif; ?>
+
+                            <?php if ( of_get_option('linkedin_url_text') != '' ): ?>
+                            <li>
+                                <a href="<?php echo of_get_option('linkedin_url_text'); ?>" class="btn-social btn-outline" target="_blank">
+                                    <i class="fa fa-fw fa-linkedin"></i>
+                                </a>
                             </li>
-                            <li><a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
-                            </li>
+                            <?php endif; ?>
+
                         </ul>
                     </div>
                     <div class="footer-col col-md-4">
